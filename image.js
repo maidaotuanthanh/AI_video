779dd24f60f1170b8c39af9e4dfb675e9b9049c3
mdtthanh <maithanh0131@gmail.com>
2024-06-23 00:32:17 +0700

    first commit

---
+++ image.js
@@ -0,0 +1,14 @@
+const imageData = [
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStl6fP_rS6PwQZFGr83R0eR3dv58KTmIVEOg&s',
+      name: 'Image One',
+    },
+    {
+      url: 'https://ichef.bbci.co.uk/images/ic/1200xn/p0dwxr5t.jpg',
+      name: 'Image Two',
+    },
+    {
+      url: 'https://ichef.bbci.co.uk/images/ic/976xn/p0ht9gpj.jpg',
+      name: 'Image Three',
+    },
+  ];
